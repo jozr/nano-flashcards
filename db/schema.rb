f9226fd968ad6e90e177add2af52f6747119abe4
jozwright <jozwright@gmail.com>
2014-12-29 23:30:44 -0800

    clean start for data mapping

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229045035) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "photos", force: :cascade do |t|
-    t.string   "avatar"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,6 +1,1 @@
 require 'spec_helper'
-
-describe Photo do
-  it { should validate_presence_of :avatar }
-  it { should validate_presence_of :created_at }
-end
