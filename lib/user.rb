242acaa5c06d1d42663ceea6794bdbe96a5f650a
jozwright <jozwright@gmail.com>
2015-01-07 21:15:00 -0800

    validate email input has @ and .[a-z]

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,6 @@
 require 'sinatra/activerecord'
 
 class User < ActiveRecord::Base
-  validates :email, :presence => true
+  validates :email, :presence => true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, on: :create }
   validates :password, :presence => true
 end
